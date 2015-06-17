6b4ef6732d655405396babc3feda3cc53d965c96
Ben Brook <bbrook154@gmail.com>
2015-06-16 20:57:46 -0700

    Add default forms

--- client/templates/bikes/parts_list.js
+++ client/templates/bikes/parts_list.js
@@ -2,16 +2,34 @@ var arr = new ReactiveArray([
 	{
 		"name": "Frame",
 		"type": ""
+	},
+	{
+		"name": "Derailleurs",
+		"type": ""
+	},
+	{
+		"name": "Crank",
+		"type": ""
+	},
+	{
+		"name": "Wheels",
+		"type": ""
+	},
+	{
+		"name": "Tires",
+		"type": ""
 	}
 ]);
 
 Template.partsList.events({
 	'click #btn-part-input-add': function(e) {
 		e.preventDefault();
+		
 		arr.push({
 			"name": $('#part-name').val(),
 			"type": $('#part-type').val()
 		});
+
 		$('#part-name').val('');
 		$('#part-type').val('');
 		console.log(arr.array());
