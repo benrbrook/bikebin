887687f78f4e77aa826178abb4819838f9b8ae1a
Ben Brook <bbrook154@gmail.com>
2015-07-26 13:24:17 -0700

    Update server comments

--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,10 @@
+// Returns a specific bike from the id
 Meteor.publish('bike', function(id) {
 	check(id, String);
 	return Bikes.find(id);
 });
 
+// Returns all bikes owned by a certain user
 Meteor.publish('ownedBikes', function(userId) {
 	check(userId, String);
 	// console.log(userId);
