ace420bd9750a5a43560e3afd41b31333b5757bf
Ben Brook <bbrook154@gmail.com>
2015-07-26 15:15:18 -0700

    Add basic star functionality
    Users can now star bikes and the values will dynamically update.

--- client/templates/bikes/bike.js
+++ client/templates/bikes/bike.js
@@ -5,5 +5,33 @@ Template.bike.helpers({
 	},
 	ownPost: function() {
 		return this.userId === Meteor.userId();
-	}
+	},
+	loggedIn: function() {
+		return Meteor.userId();
+	},
+	// Updates the class to change the star glyphcon
+	starredGlyph: function() {
+		var userId = Meteor.userId();
+		if (userId && !_.include(this.starUsers, userId)) {
+			return '-empty';
+		} else {
+			return '';
+		}
+	}//,
+	// starredDisabled: function() {
+	// 	var userId = Meteor.userId();
+	// 	if (userId && !_.include(this.starUsers, userId)) {
+	// 		return '';
+	// 	} else {
+	// 		return 'disabled';
+	// 	}
+	// }
 });
+
+Template.bike.events({
+	'click #btn-bike-star': function(e) {
+		e.preventDefault();
+		console.log(this.votes);
+    	Meteor.call('star', this._id);
+	}
+})
