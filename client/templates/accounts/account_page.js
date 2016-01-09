0d52bb7b076e4fb665a44bedf8153bcc72c89569
Ben Brook <bbrook154@gmail.com>
2016-01-08 20:45:21 -0800

    Update share url

--- client/templates/accounts/account_page.js
+++ client/templates/accounts/account_page.js
@@ -6,5 +6,18 @@ Template.accountPage.helpers({
 	},
 	user: function() {
 		return Meteor.users.find({_id: Meteor.userId});
+	},
+	options: function() {
+		return {
+			type: 'text',
+	    	async: true,
+	    	position: 'top',
+	    	value: Session.get('text'),
+	    	emptyText: "Add your location",
+	    	onsubmit: function(val, cb) {
+        		Session.set('text', val);
+        		cb();
+	  		}
+		};
 	}
-})
+});
